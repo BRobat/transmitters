a0e61cf30c3494a4d58daf0293dabec90416fabd
BRobat <Patogeen@icloud.com>
2018-07-10 22:07:58 +0200

    init

---
+++ main.js
@@ -0,0 +1,76 @@
+function setNumberOfTransmitters() {
+    let parent = document.getElementById("parentElement");
+    let value = document.getElementById("numberOfTransmitters").value;
+    console.log(value);
+
+    for (let i = 1; i <= value; i++) {
+        let div = document.createElement("div");
+        let label = document.createElement("label");
+        let inputX = document.createElement("input");
+        let inputY = document.createElement("input");
+        let inputPower = document.createElement("input");
+        
+        label.setAttribute("id","lbl"+i);
+        inputX.setAttribute("id","inputX"+i);
+        inputY.setAttribute("id","inputY"+i);
+        inputPower.setAttribute("id","inputPower"+i);
+
+        inputX.setAttribute("class","inputX");
+        inputY.setAttribute("class","inputY");
+        inputPower.setAttribute("class","inputPower");
+
+        label.innerText = "Transmitter no.: " + i;
+        inputX.setAttribute("placeholder", "x");
+        inputY.setAttribute("placeholder", "y");
+        inputPower.setAttribute("placeholder", "power");
+
+        inputX.setAttribute("type", "number");
+        inputY.setAttribute("type", "number");
+        inputPower.setAttribute("type", "number");
+        
+        parent.appendChild(div);
+        div.appendChild(label);
+        div.appendChild(inputX);
+        div.appendChild(inputY);
+        div.appendChild(inputPower);
+    }
+}
+
+function getDataFromInputs() {
+    let inputLen = document.getElementsByClassName("inputX").length;
+    let inputsX = document.getElementsByClassName("inputX");
+    let inputsY = document.getElementsByClassName("inputY");
+
+    for(let i = 0; i < inputLen; i++) {
+        
+        console.log(inputsX[i].value);
+    }   
+}
+
+    // point x, point y, transmitter x, transmitter y, transmitter power
+function checkIfPointIsInside(px, py, tx, ty, tp){
+    // checks if given point lies within givn circle.
+    let y = Math.sqrt(Math.pow(py - ty));
+    let x = Math.sqrt(Math.pow(px - tx));
+    let p = Math.sqrt(Math.pow(x + y));
+    if (p <= tp){
+        return true;
+    }
+}
+    // circles
+function giveGreenLight(c1, c2) {
+    // checks if the two circles intersect and then sets the other one as the one as good to go.
+    let x = Math.sqrt(Math.pow(c1.x - c2.x));
+    let y = Math.sqrt(Math.pow(c1.y - c2.y));
+    let p = Math.sqrt(Math.pow(x + y));
+    let cp = c1.p + c2.p;
+    if (p <= cp) {
+        c1.greenLight = true;
+        c2.greenLight = true;
+    }
+}
+
+function createObjects() {
+    // creates object
+}
+
---
+++ todo.md
@@ -0,0 +1,49 @@
+### To do  
+
+## Description
+Celem zadania jest napisanie programu, który dla danej mapy nadajników oceni, czy jest możliwy bezpieczny przelot pomiędzy dwoma zadanymi punktami poligonu – początkowym i końcowym. (Przelot jest bezpieczny, jeżeli istnieje trasa, będąca ciągłą krzywą, od punktu początkowego do punktu końcowego, przebiegająca wyłącznie przez obszary pokryte zasięgiem nadajników.)
+
+Poligon jest reprezentowany przez dodatnią ćwiartkę układu współrzędnych.
+Na poligonie znajduje się co najmniej jeden nadajnik. Lokalizacja każdego nadajnika jest zadana przez parę dodatnich współrzędnych całkowitych (x, y), natomiast jego moc przez liczbę całkowitą dodatnią m. Zasięg nadajnika jest wyznaczony przez koło (wraz z brzegiem) o środku w punkcie (x, y) i promieniu m.
+Zarówno punkt początkowy, jak i punkt końcowy są zadane przez parę dodatnich współrzędnych całkowitych.
+
+## Main  objectives
+1. Program wczytuje (uytkownik podaje) liczbę nadajników
+2. Dla kadego nadajnika program wczytuje jego współrzędne oraz moc
+3. Program wczytuje współrzędne punktu początkowego.
+4. Program wczytuje współrzędne punktu końcowego.
+5. Program oblicza, czy istnieje bezpieczny przelot i zwraca odpowiedź: „bezpieczny przelot jest możliwy” lub „bezpieczny przelot nie jest możliwy”.
+
+## Example 
+
+input:
+liczba nadajników: 3
+współrzędna x nadajnika nr. 1: 3
+współrzędna y nadajnika nr. 1: 5
+moc nadajnika nr. 1: 2
+współrzędna x nadajnika nr. 2: 4
+współrzędna y nadajnika nr. 2: 6
+moc nadajnika nr. 2: 1
+współrzędna x nadajnika nr. 3: 3
+współrzędna y nadajnika nr. 3: 6
+moc nadajnika nr. 3: 3
+współrzędna x punktu początkowego: 3
+współrzędna y punktu początkowego: 5
+współrzędna x punktu końcowego: 2
+współrzędna y punktu końcowego: 6
+
+
+output:
+
+
+## Minor objectives
+1. funkcja tworząca podaną ilość inputów
+2. funkcja zbierająca dane z inputów
+3. 
+4. 
+5. 
+6. 
+7. 
+8. 
+9. 
+10. 
